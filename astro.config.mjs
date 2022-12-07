c4a9604b99ec4c994b8c62127840acaecbd11df5
ruben00001 <a.ruben00001@gmail.com>
2022-12-07 17:49:39 +0000

    image scroll desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), image({ serviceEntryPoint: "@astrojs/image/sharp" })],
 });
