cbc9340a38be2fb347c7c196a02765c34ebffdbc
ruben00001 <a.ruben00001@gmail.com>
2022-12-09 14:03:54 +0000

    multiple youtube videos

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,18 @@ export type LocalImage = {
   type: "image";
   url: string;
 };
+
+export type VideoCoverImage = {
+  coverImage?: {
+    url: string;
+    position?: "top" | "center" | "bottom";
+  };
+};
+
 export type YoutubeVideo = {
   type: "youtube-video";
   youtubeUrl: string;
-};
+} & VideoCoverImage;
 
 export type Page = {
   title: string;
