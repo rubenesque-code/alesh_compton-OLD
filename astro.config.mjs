6ae6211fecf6c60b8d86d97fe1f74c5a76184c1c
ruben00001 <a.ruben00001@gmail.com>
2022-12-10 14:43:42 +0000

    site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), image({ serviceEntryPoint: "@astrojs/image/sharp" })],
+  integrations: [image({ serviceEntryPoint: "@astrojs/image/sharp" })],
+  site: "https://joshua-gordon.netlify.app/",
+  base: "/",
+  publicDir: "./public",
 });
