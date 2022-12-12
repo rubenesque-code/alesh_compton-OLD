398804aa4600778ddd04f89d7e22736ada47389b
ruben00001 <a.ruben00001@gmail.com>
2022-12-12 16:41:06 +0000

    rm vol message if vol changed

--- src/helpers/device.ts
+++
@@ -1,8 +0,0 @@
-export function checkIsTouchDevice() {
-  return (
-    "ontouchstart" in window ||
-    navigator.maxTouchPoints > 0 ||
-    // @ts-ignore
-    navigator.msMaxTouchPoints > 0
-  );
-}
