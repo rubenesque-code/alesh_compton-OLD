d01c187ec182bebcec7bc776ed0a8e8463bc4d8a
ruben00001 <a.ruben00001@gmail.com>
2022-12-12 15:27:30 +0000

    check yt controls vol changeable

---
+++ src/helpers/device.ts
@@ -0,0 +1,8 @@
+export function checkIsTouchDevice() {
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    // @ts-ignore
+    navigator.msMaxTouchPoints > 0
+  );
+}
