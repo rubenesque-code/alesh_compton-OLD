45770c44ef6cb8d735f7227f0b06ffe711d6c941
ruben00001 <a.ruben00001@gmail.com>
2022-12-13 16:44:10 +0000

    add findup node package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import { astroImageTools } from "astro-imagetools";
 
-import sitemap from "@astrojs/sitemap";
+// NOTE: astro-imagetools fails build when uninstall astro/image and sharp in this project. This is not the case in the tutorial app.
 
+// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
