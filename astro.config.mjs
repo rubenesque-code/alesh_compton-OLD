a5f6e1d05803a2bee3f675a5349178cf4fc28692
ruben00001 <a.ruben00001@gmail.com>
2022-12-13 15:58:29 +0000

    image config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sitemap(),
-    image({
+    /*     image({
       serviceEntryPoint: "@astrojs/image/sharp",
-    }),
+    }), */
     astroImageTools,
   ],
   site: "https://joshua-gordon.netlify.app/",
